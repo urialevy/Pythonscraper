ee0dd07dd14bf07ea05598d6ea1453bd2be98999
Uria <uria@urialevy.com>
2023-11-10 19:36:22 +0000

    change source, Amazon didn't like the scraper

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+from typing import Union
+from typing import  List
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
 
 app = FastAPI()
 
+def create_url(month:str,day:int) -> str:
+    url = f"https://www.onthisday.com/day/{month}/{day}"
+    return url
 
-@app.get("/{asin}")
-def get_data(asin: str):
-    session = requests.Session()
-    session.headers.update({
-        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
-    })
-    resp = session.get("https://google.com")
-    soup = BeautifulSoup(resp.text, "html.parser")
-    data = {
-        "asin": asin,
-        "name": soup.select_one("h1#title").text,
-        "price": soup.select_one("span.a-offspring").text
-    }
-    return{"results":data}
+def retrieve_page(url:str) -> BeautifulSoup:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup
+
+def events_of_the_day(month:str,day:int) -> List[str]:
+    url = create_url(month, day)
+    page = retrieve_page(url)
+    raw_events = page.find_all(class_ = "event")
+    events = [event.text for event in raw_events]
+    return events
+
